6d5c9d1163ed6fb79124da607229f987d36a0b5c
Serkan Algur <info@wpadami.com>
2020-11-22 00:58:57 +0400

    :tada: Fixes

--- time.go
+++ time.go
@@ -6,10 +6,24 @@ import "time"
 * BASIC TIME FUNCTIONS *
 ************************/
 
+/**
+*	Similar function of time() in PHP.
+* Original : https://www.php.net/manual/en/function.time.php
+* func Time
+* return current Unix timestamp
+**/
+
 // Time Function
 func Time() int64{
 	return time.Now().Unix()
 }
+
+/**
+*	Similar function of time() in PHP.
+* Original : https://www.php.net/manual/en/function.time.php
+* func Now
+* return current Unix timestamp
+**/
 
 // Now Function
 func Now() int64{
