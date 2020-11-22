ff7bc58c940aa7411df4fcd593038696f28048dd
Serkan Algur <info@wpadami.com>
2020-11-22 18:20:19 +0400

    ArrayCountValues

--- array_related_functions.go
+++ array_related_functions.go
@@ -18,7 +18,28 @@ func ArrayKeys(v map[string]interface{}) []string {
 
 // ArrayPush - Add an element to end of given array
 func ArrayPush(v *[]interface{}, data ...interface{}) {
-
 		*v = append(*v, data...)
+}
 
+// ArrayReverse - Reverse an array
+func ArrayReverse(v []interface{}) []interface{} {
+		for ind, fnd:= 0, len(v); ind < fnd; ind, fnd = ind+1, fnd-1 {
+			v[ind], v[fnd] = v[fnd], v[ind]
+		}
+		return v
+}
+
+// ArrayCountValues - Count an arrays values
+func ArrayCountValues(v []interface{}) map[interface{}]uint {
+
+		cnt := make(map[interface{}]uint)
+		for _,s:= range v {
+			if c, ok := cnt[s]; ok {
+				cnt[s] = c+1
+			} else {
+				cnt[s] = 1
+			}
+		}
+
+		return cnt
 }
