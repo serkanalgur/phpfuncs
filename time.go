2fdc8864a18c2960e9a33ab6ca651d5c9a8f5add
serkanalgur <info@wpadami.com>
2020-11-24 16:03:01 +0400

    Time

--- time.go
+++ time.go
@@ -6,34 +6,26 @@ import "time"
 * BASIC TIME FUNCTIONS *
 ************************/
 
-/**
-* Time
-* Similar function of time() in PHP.
-* Original : https://www.php.net/manual/en/function.time.php
-* func Time
-* return current Unix timestamp
-**/
+// Time - Similar function of time() in PHP.
+// Original : https://www.php.net/manual/en/function.time.php
 func Time() int64 {
 	return time.Now().Unix()
 }
 
-/**
-* Now
-* Similar function of time() in PHP.
-* Original : https://www.php.net/manual/en/function.time.php
-* func Now
-* return current Unix timestamp
-**/
+// Now - Similar function of time() in PHP.
+// Original : https://www.php.net/manual/en/function.time.php
 func Now() int64 {
 	return time.Now().Unix()
 }
 
 // Sleep - Delay in seconds
+// Original : https://www.php.net/manual/en/function.sleep.php
 func Sleep(t int64) {
 	time.Sleep(time.Duration(t) * time.Second)
 }
 
 // USleep - Delay in microseconds
+// Original : https://www.php.net/manual/en/function.usleep.php
 func USleep(t int64) {
 	time.Sleep(time.Duration(t) * time.Microsecond)
 }
