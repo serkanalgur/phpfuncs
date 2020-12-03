0552c167af5356fa7cdd92f895338c0e47315cd2
Serkan Algur <info@wpadami.com>
2020-12-04 03:28:48 +0400

    in_array fix

--- array_related_functions.go
+++ array_related_functions.go
@@ -139,3 +139,24 @@ func IsArray(v interface{}) bool {
 	}
 	return false
 }
+
+// InArray - Similar function of in_array in PHP
+// Original : https://www.php.net/manual/en/function.in-array.php
+// func inArray
+// needle : string, int
+// haystack : should be an array
+// strict : set true for type check
+// return boolean true / false
+func InArray(needle interface{}, haystack interface{}) bool {
+	switch reflect.TypeOf(haystack).Kind() {
+	default:
+		s := reflect.ValueOf(haystack)
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(needle, s.Index(i).Interface()) == true {
+				return true
+			}
+		}
+	}
+	return false
+
+}
