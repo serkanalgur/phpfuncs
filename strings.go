d270cfaf80df5b382979c4b837bbac46f393e163
Serkan Algur <info@wpadami.com>
2020-12-06 04:11:55 +0400

    Str Functions

--- strings.go
+++ strings.go
@@ -44,6 +44,12 @@ func Explode(s,set string) []string {
 	return strings.Split(s,set)
 }
 
+// Join - Alias o Implode
+// Original : https://www.php.net/manual/en/function.join.php
+// Alias o Implode
+func Join(set string, s []string) string {
+	return Implode(set,s)
+}
 
 // Ltrim - Strip whitespace (or other characters) from the beginning of a string
 // Original : https://www.php.net/manual/en/function.ltrim.php
@@ -59,6 +65,35 @@ func Ltrim(s, set string) string{
 func Rtrim(s, set string) string {
 	if set == "" { set = " "}
 	return strings.TrimRight(s, set)
+}
+
+// Strtolower - Make a string lowercase
+// Original : https://www.php.net/manual/en/function.strtolower.php
+// Returns string with all alphabetic characters converted to lowercase.
+func Strtolower(s string) string {
+	return strings.ToLower(s)
+}
+
+
+// Strtoupper - Make a string uppercase
+// Original : https://www.php.net/manual/en/function.strtoupper.php
+// Returns string with all alphabetic characters converted to uppercase.
+func Strtoupper(s string) string {
+	return strings.ToUpper(s)
+}
+
+// MbStrtolower - Make a string lowercase
+// Original : https://www.php.net/manual/en/function.mb-strtolower.php
+// Returns str with all alphabetic characters converted to lowercase.
+func MbStrtolower(s string) string {
+	return Strtolower(s)
+}
+
+// MbStrtoupper - Make a string uppercase
+// Original : https://www.php.net/manual/en/function.mb-strtoupper.php
+// Returns str with all alphabetic characters converted to uppercase.
+func MbStrtoupper(s string) string {
+	return Strtoupper(s)
 }
 
 // Trim - Strip whitespace (or other characters) from the beginning and end of a string
